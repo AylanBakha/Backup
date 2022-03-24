0f527561bc6331fe74b826e3b9ac6e0e8a019910
gitman <90602482+AylanBakha@users.noreply.github.com>
2022-03-24 10:05:35 +0100

    Add files via upload

---
+++ Backup Situation Professionnelle 2 V2/GSB/vues/v_listeFraisHorsForfait.php
@@ -0,0 +1,79 @@
+﻿
+<table class="listeLegere">
+  	   <caption>Descriptif des éléments hors forfait
+       </caption>
+             <tr>
+                <th class="date">Date</th>
+				        <th class="libelle">Libellé</th>
+                <th class="paiement">Paiement</th>
+                <th class="montant">Montant</th>  
+                <th class="action">&nbsp;</th>              
+             </tr>
+          
+    <?php    
+	    foreach( $lesFraisHorsForfait as $unFraisHorsForfait) 
+		{
+			$libelle = $unFraisHorsForfait['libelle'];
+			$date = $unFraisHorsForfait['date'];
+      $paiement = $unFraisHorsForfait['paiement'];
+			$montant=$unFraisHorsForfait['montant'];
+			$id = $unFraisHorsForfait['id'];
+	?>		
+            <tr>
+                <td><?php echo $date ?></td>
+                <td><?php echo $libelle ?></td>
+                <td><?php echo $paiement ?></td>
+                <td><?php echo $montant ?></td>
+                <td><a href="index.php?uc=gererFrais&action=supprimerFrais&idFrais=<?php echo $id ?>" 
+				onclick="return confirm('Voulez-vous vraiment supprimer ce frais?');">Supprimer ce frais</a></td>
+             </tr>
+	<?php		 
+          
+          }
+	?>	  
+                           
+    </table>
+      <form action="index.php?uc=gererFrais&action=validerCreationFrais" method="post">
+      <div class="corpsForm">
+         
+          <fieldset>
+            <legend>Nouvel élément hors forfait
+            </legend>
+            <p>
+              <label for="txtDateHF">Date (jj/mm/aaaa) : </label>
+              <input type="text" id="txtDateHF" name="dateFrais" size="10" maxlength="10" value=""  />
+            </p>
+            <p>
+              <label for="txtLibelleHF">Libellé : </label>
+              <input type="text" id="txtLibelleHF" name="libelle" size="70" maxlength="256" value="" />
+            </p>
+            <p>
+              <label for="txtPaiementHF">Paiement : </label>
+              <select name=" "  id=" " required>
+			            <?php $reponse = $bdd->query('SELECT paiement FROM "LigneFraisHorsForfait"');
+                              while ($donnees = $reponse->fetch())
+									{
+									?>
+					        <option value="<?php echo $donnees['paiement']; ?>"> 
+					        <?php echo $donnees['paiement']; ?>
+					        </option>
+				         	<?php } ?>
+		            </select>
+              <input type="select" id="txtPaiementHF" name="paiement" value="" from ModePaiement/>
+              
+            </p>
+            <p>
+              <label for="txtMontantHF">Montant : </label>
+              <input type="text" id="txtMontantHF" name="montant" size="10" maxlength="10" value="" />
+            </p>
+          </fieldset>
+      </div>
+      <div class="piedForm">
+      <p>
+        <input id="ajouter" type="submit" value="Ajouter" size="20" />
+        <input id="effacer" type="reset" value="Effacer" size="20" />
+      </p> 
+      </div>
+        
+      </form>
+  </div>
---
+++ Site/CRUD/add.php
@@ -0,0 +1,39 @@
+<?php
+require_once('connect.php');
+
+if(isset($_POST)){
+    if(isset($_POST['produit']) && !empty($_POST['produit'])
+        && isset($_POST['prix']) && !empty($_POST['prix'])
+        && isset($_POST['nombre']) && !empty($_POST['nombre'])){
+            $produit = strip_tags($_POST['produit']);
+            $prix = strip_tags($_POST['prix']);
+            $nombre = strip_tags($_POST['nombre']);
+
+            $sql = "INSERT INTO `liste` (`id`, `produit`, `prix`, `nombre`) VALUES (id, produit, prix, nombre);";
+
+            $query = $db->prepare($sql);
+
+            $query->bindValue(':produit', $produit, PDO::PARAM_STR);
+            $query->bindValue(':prix', $prix, PDO::PARAM_STR);
+            $query->bindValue(':nombre', $nombre, PDO::PARAM_INT);
+
+            $query->execute();
+            $_SESSION['message'] = "Produit ajouté avec succès !";
+            header('Location: index.php');
+        }
+}
+
+require_once('close.php');
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<form method="post">
+    <label for="produit">Produit</label>
+    <input type="text" name="produit" id="produit">
+    <label for="prix">Prix</label>
+    <input type="text" name="prix" id="prix">
+    <label for="nombre">Nombre</label>
+    <input type="number" name="nombre" id="nombre">
+    <button>Enregistrer</button>
+</form>
